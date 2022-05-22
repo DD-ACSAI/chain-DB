9b6811ac5c4447b4ff0ec5e34a5233c808e5c523
dario-loi <77234595+dario-loi@users.noreply.github.com>
2022-05-22 15:05:06 +0200

    New Tokens

--- DBapplication/DBapplication/src/manager/DBmanager.cpp
+++ DBapplication/DBapplication/src/manager/DBmanager.cpp
@@ -1,4 +1,4 @@
 #include "DBmanager.h"
 
 DBcontext DBmanager::context = DBcontext::DIR_TREE;
-std::unordered_set<std::string> const DBmanager::SQLtokens = { "SELECT", "FROM", "WHERE", "ORDER BY", "GROUP BY", "SUM", "AVG" };
+std::unordered_set<std::string> const DBmanager::SQLtokens = { "SELECT", "FROM", "WHERE", "ORDER BY", "GROUP BY", "SUM", "AVG", "DROP", "SCHEMA", "CASCADE", "CALL", "AS", "JOIN", "ON"};
