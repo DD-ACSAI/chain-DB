5bb4cdb33d29f1e200e919fc57c3907fade885ca
Davide Marincione <76567598+Davide-F5-Marincione@users.noreply.github.com>
2022-06-05 00:29:15 +0200

    final version uploded to the moodle

---
+++ Loi_Marincione_DB_project/program_and_codebase/codebase/DBapplication/src/defines/coninfo.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string_view>
+
+
+#ifdef DB
+
+#define DBSTRING "dbname = " DB
+
+#else
+
+#error Database name must be defined!
+
+#endif // DB
+
+#ifdef PASSWORD
+
+#define PWSTR " password = " PASSWORD
+
+#else 
+
+#define PWSTR ""
+
+#endif // PASSWORD
+
+#ifdef USER
+
+#define USERSTR " user = " USER
+
+#else 
+
+#define USERSTR ""
+
+#endif // USER
+
+#define CONNECT_QUERY DBSTRING PWSTR USERSTR
---
+++ Loi_Marincione_DB_project/program_and_codebase/codebase/DBapplication/src/manager/Pathfinder.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "libpq-fe.h"
+#include "cstdint"
+
+
+namespace paths
+{
+	enum VehicleType : char
+	{
+		PLANE = 1,
+		SHIP = 2,
+		CAR = 4
+	};
+
+	struct pathOptions
+	{
+
+		char allowedVehicles;
+
+	};
+
+	struct destination
+	{
+		VehicleType vehicle;
+		int64_t from;
+		int64_t to;
+		double distance;
+		double fee;
+		double heuristic;
+
+	public:
+		inline destination(paths::VehicleType const vehicleType, int64_t const from_code, int64_t const to_code, double const distance_val, double const fee, double const heuristic) 
+			: vehicle(vehicleType), from(from_code), to(to_code), distance(distance_val), fee(fee), heuristic(heuristic) {}
+		destination(destination const& other) = default;
+	};
+}
+
+class Pathfinder
+{
+public:
+
+	Pathfinder(PGconn*& conn, PGresult*& res) : conn(conn), res(res) {};
+	void pathfind(int64_t from_code, int64_t to_code, int64_t client, short special_case=0);
+
+private:
+	PGconn* conn;
+	PGresult* res;
+
+};
